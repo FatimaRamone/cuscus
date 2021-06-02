534989a432ff9a15871d749d939cb7d8f2540e2e
Fatima Ramone <fatimapitaemail@gmail.com>
2021-06-02 22:28:58 +0200

    another commmit

---
+++ app/index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const MongoClient = require('mongodb').MongoClient;
+const express = require('express');
+
+// Constants
+const PORT = 127.0.0.1:5000;
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello world\n');
+});
+
+app.listen(PORT);
+console.log(`Running on Port:${PORT}`);
